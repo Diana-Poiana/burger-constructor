4a0fea8e6069f87ca995270e603fad5a9879727d
Diana Ivanova <diana.website.developer@gmail.com>
2024-01-23 15:14:44 +0000

    sections done

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 window.addEventListener('DOMContentLoaded', () => {
-  // header navigation links
-  const profileMenuBtn = document.querySelector('.header__profile-menu');
-  const profileMenu = document.querySelector('.header__drop-menu');
 
   // to show and hide different sections
   const makeBurgerBtn = document.querySelector('.hero__make-burger-btn');
@@ -10,25 +7,23 @@ window.addEventListener('DOMContentLoaded', () => {
   const heroSection = document.querySelector('.hero');
   const burgerConstructorSection = document.querySelector('.burger-constructor');
   const checkoutModalWindow = document.querySelector('.checkout');
-  const checkoutBtn = document.querySelector('.burger-constructor__checkout');
-
-  const ketchupBtn = document.getElementById('ketchup');
-  const ketchupIngridient = document.querySelector('.burger-constructor__ketchup');
+  const checkoutBtns = document.querySelectorAll('.burger-constructor__checkout');
 
   // ingridients section
-  const btnsContainer = document.querySelectorAll('.ingridients__qty-choice');
-  const qtyInputs = document.querySelectorAll('.ingridients__qty');
   const btnMinusList = document.querySelectorAll('.ingridients__btn-minus');
   const btnPlusList = document.querySelectorAll('.ingridients__btn-plus');
-
   const burgerNewIngridientContainer = document.querySelector('.burger-constructor__content');
-
   const warningMessage = document.querySelector('.burger-constructor__warning');
 
-  const burgerPrice = document.querySelector('.burger-constructor__price');
-  const minutesToCook = document.querySelector('.burger-constructor__summary-min');
-  const ingridientWeight = document.querySelector('.burger-constructor__summary-weight');
-  const ingridientKcal = document.querySelector('.burger-constructor__summary-calories');
+  // summary
+  const burgerPriceBlocks = document.querySelectorAll('.burger-constructor__price');
+  const minutesToCook = document.querySelector('.summary-list__min');
+  const ingridientWeight = document.querySelector('.summary-list__weight');
+  const ingridientKcal = document.querySelector('.summary-list__calories');
+
+  // ketchup btn
+  const ketchupIngridient = document.querySelector('.burger-constructor__ketchup');
+  const ketchupBtns = document.querySelectorAll('.ketchup-btn');
 
   // checkout section
   const modalCloseBtn = document.querySelectorAll('.cancel');
@@ -36,35 +31,22 @@ window.addEventListener('DOMContentLoaded', () => {
   const modalWindowContent = document.querySelector('.checkout__content');
   const modalWindowConfirmation = document.querySelector('.checkout__finished');
 
+  // footer
+  const footer = document.querySelector('.footer');
+
   // drop menu
   const dropMenuDesktop = document.getElementById('desktop-drop-menu');
   const dropMenuMobile = document.getElementById('mobile-drop-menu');
   const dropMenuDesktopBtn = document.getElementById('desktop-menu-btn');
   const dropMenuMobileBtn = document.getElementById('mobile-menu-btn');
 
-  // const burgerMenuBtn = document.querySelector('.mobile-menu__burger-btn');
-  // const mobileMenu = document.querySelector('.mobile-menu__list');
-
-  const imgsContainer = document.querySelector('.burger-constructor__imgs-container');
-
-  const imgsContainerHeight = imgsContainer.clientHeight;
-  console.log(imgsContainerHeight);
+  // for animeted ingridients
   let currentBottomValue = 5;
-  let previousIngridient;
-  let temporaryBottom;
   let tomatoCount = 0;
   let cucumberCount = 0;
   let onionCount = 0;
-  let cutlet = 0;
-  let mayo = 0;
-  let onion = 0;
-  let tomato = 0;
-  let cucumber = 0;
-  let cheese = 0;
-  let salad = 0;
-  let bun = 0;
 
-
+  // for summary block
   let totalPrice = 0;
   let totalCookingTime = 0;
   let totalWeight = 0;
@@ -83,6 +65,22 @@ window.addEventListener('DOMContentLoaded', () => {
     topbun: { cookingMin: 1, kcal: 56, weightGrams: 20, price: 0 }
   };
 
+  // check user's screen width
+  let screenWidth;
+  function updateScreenWidth() {
+    screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    console.log("Screen width: " + screenWidth + "px");
+  }
+
+  updateScreenWidth();
+
+  // toggle menu
+  function toggleMenu(menu, btn) {
+    menu.classList.toggle('active');
+    btn.classList.toggle('active');
+  }
+
+  // show/hide div's
   function showBurgerConstructor() {
     heroSection.style.display = 'none';
     burgerConstructorSection.style.display = 'block';
@@ -101,11 +99,6 @@ window.addEventListener('DOMContentLoaded', () => {
     newElem.classList.add(ingridient);
     return newElem;
   }
-
-
-  let screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-  console.log("Ширина экрана: " + screenWidth + " пикселей");
-
 
   // plus ingridient
   function plusIngridient(ingridient) {
@@ -184,6 +177,11 @@ window.addEventListener('DOMContentLoaded', () => {
       }
 
       else if (ingridient === 'bun') {
+        newElem.style.left = 30 + 'px';
+        newElem.style.bottom = currentBottomValue + 30 + 'px';
+      }
+
+      else if (ingridient === 'topbun') {
         newElem.style.left = 30 + 'px';
         newElem.style.bottom = currentBottomValue + 30 + 'px';
       }
@@ -278,7 +276,7 @@ window.addEventListener('DOMContentLoaded', () => {
     checkIfNotTooBigBurger();
   }
 
-  // delete ingridient
+  // minus ingridient
   function minusIngridient(ingridient) {
     const elementsToRemove = burgerNewIngridientContainer.querySelectorAll(`[data-ingridient="${ingridient}"]`);
     const lastElement = elementsToRemove[elementsToRemove.length - 1];
@@ -291,6 +289,42 @@ window.addEventListener('DOMContentLoaded', () => {
       return;
     }
     checkIfNotTooBigBurger();
+  }
+
+  // adding ingridient animation
+  function animation(element) {
+
+    if (screenWidth > 626) {
+      const animation = element.animate(
+        [
+          { transform: 'translate(0)' },
+          { transform: 'translate(0, 630px)' }
+        ],
+        {
+          duration: 500
+        }
+      );
+
+      animation.addEventListener('finish', function () {
+        element.style.transform = 'translate(0, 630px)';
+      });
+    }
+    else if (screenWidth <= 626) {
+      const animation = element.animate(
+        [
+          { transform: 'translate(0)' },
+          { transform: 'translate(0, 316px)' }
+        ],
+        {
+          duration: 500
+        }
+      );
+
+      animation.addEventListener('finish', function () {
+        element.style.transform = 'translate(0, 316px)';
+      });
+    }
+
   }
 
   // update ingridient positions
@@ -338,6 +372,7 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+  // check ingridients total number
   function checkIfNotTooBigBurger() {
     const elementsToCheck = burgerNewIngridientContainer.querySelectorAll('[data-ingridient]');
     const elementsArray = Array.from(elementsToCheck);
@@ -351,54 +386,11 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-
-
-  // adding ingridient animation
-  function animation(element) {
-
-    if (imgsContainerHeight >= 627) {
-      const animation = element.animate(
-        [
-          { transform: 'translate(0)' },
-          { transform: 'translate(0, 630px)' }
-        ],
-        {
-          duration: 500
-        }
-      );
-
-      animation.addEventListener('finish', function () {
-        element.style.transform = 'translate(0, 630px)';
-      });
-    } else if (imgsContainerHeight < 627) {
-
-      let difference = imgsContainerHeight - 740;
-      console.log(difference);
-      let translateY = 630 + difference;
-      const animation = element.animate(
-        [
-          { transform: 'translate(0)' },
-          { transform: `translate(0, ${translateY}px)` }
-        ],
-        {
-          duration: 500
-        }
-      );
-
-      animation.addEventListener('finish', function () {
-        element.style.transform = `translate(0, ${translateY}px)`;
-      });
-    }
-
-  }
-
-
   // update plusBtn value
   function updateInputPlus(btn) {
     const inputElement = btn.closest('.ingridients__qty-choice').querySelector('input');
     const currentValue = parseInt(inputElement.value);
     inputElement.value = isNaN(currentValue) ? 1 : currentValue + 1;
-    console.log(currentValue);
   }
 
   // update minusBtn value
@@ -447,7 +439,9 @@ window.addEventListener('DOMContentLoaded', () => {
     minutesToCook.textContent = totalCookingTime;
     ingridientWeight.textContent = totalWeight;
     ingridientKcal.textContent = totalKcal;
-    burgerPrice.textContent = totalPrice.toFixed(2);
+    burgerPriceBlocks.forEach((priceBlock) => {
+      priceBlock.textContent = totalPrice.toFixed(2);
+    })
   }
 
   // delete single ingridient info from summary block
@@ -473,36 +467,34 @@ window.addEventListener('DOMContentLoaded', () => {
     minutesToCook.textContent = totalCookingTime;
     ingridientWeight.textContent = totalWeight;
     ingridientKcal.textContent = totalKcal;
-    burgerPrice.textContent = (Math.abs(totalPrice)).toFixed(2);
+    burgerPriceBlocks.forEach((priceBlock) => {
+      priceBlock.textContent = totalPrice.toFixed(2);
+    })
   }
 
-
-
-
-
-  // toggle modal window
+  // toggle checkout modal window
   let finish;
   function showModalWindow() {
     createNewIngridient('topbun');
     plusIngridient('topbun');
     finish = setInterval(() => {
       burgerConstructorSection.style.filter = 'blur(5px)';
+      footer.style.filter = 'blur(5px)';
       checkoutModalWindow.style.display = 'block';
     }, 1400);
   }
 
+  // close modal window
+
   function closeModalWindow() {
     burgerConstructorSection.style.filter = 'none';
+    footer.style.filter = 'none';
     checkoutModalWindow.style.display = 'none';
     clearInterval(finish);
   }
 
-  function toggleMenu(menu, btn) {
-    menu.classList.toggle('active');
-    btn.classList.toggle('active');
-  }
-
   // event listeners
+  window.addEventListener('resize', updateScreenWidth);
 
   dropMenuDesktopBtn.addEventListener('click', () => {
     toggleMenu(dropMenuDesktop, dropMenuDesktopBtn);
@@ -516,26 +508,30 @@ window.addEventListener('DOMContentLoaded', () => {
   makeBurgerHeaderBtn.addEventListener('click', showBurgerConstructor);
   headerLink.addEventListener('click', showMainPage);
 
-  ketchupBtn.addEventListener('click', () => {
-    addSingleIngridientInfo(ketchupIngridient);
-    const ketchupAddedNotification = document.getElementById('ketchup-added');
-    ketchupAddedNotification.style.animation = 'fadeInFromNone 2s';
-    ketchupBtn.disabled = true;
+  ketchupBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      addSingleIngridientInfo(ketchupIngridient);
+      const ketchupAddedNotification = document.getElementById('ketchup-added');
+      ketchupAddedNotification.style.animation = 'fadeInFromNone 2s';
+      btn.disabled = true;
 
-    setTimeout(function () {
-      ketchupAddedNotification.style.animation = 'none';
-      ketchupBtn.disabled = false;
-    }, 2000);
+      setTimeout(function () {
+        ketchupAddedNotification.style.animation = 'none';
+        btn.disabled = false;
+      }, 1200);
+    })
   });
 
-  checkoutBtn.addEventListener('click', showModalWindow);
-  finishBtn.addEventListener('click', () => {
-    modalWindowContent.style.display = 'none';
-    modalWindowConfirmation.style.display = 'block';
-    setInterval(() => {
-      closeModalWindow();
-    }, 3000);
-    checkoutBtn.disabled = true;
+  checkoutBtns.forEach((checkoutBtn) => {
+    checkoutBtn.addEventListener('click', showModalWindow);
+    finishBtn.addEventListener('click', () => {
+      modalWindowContent.style.display = 'none';
+      modalWindowConfirmation.style.display = 'block';
+      setInterval(() => {
+        closeModalWindow();
+      }, 3000);
+      checkoutBtn.disabled = true;
+    });
   });
 
   btnPlusList.forEach((btn) => {
@@ -557,7 +553,5 @@ window.addEventListener('DOMContentLoaded', () => {
   modalCloseBtn.forEach((btn) => {
     btn.addEventListener('click', closeModalWindow);
   });
-
-  // burgerMenuBtn.addEventListener('click', toggleMobileMenu);
 
 });
